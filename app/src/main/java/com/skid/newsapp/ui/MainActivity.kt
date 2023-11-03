2633827db5d465f1f3bd788886e7dbf609510b63
skid45 <akopovgrisha@gmail.com>
2023-11-03 13:58:14 +0300

    Added Bottom Navigation to MainActivity

--- app/src/main/java/com/skid/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/skid/newsapp/ui/MainActivity.kt
@@ -3,10 +3,29 @@ package com.skid.newsapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.skid.newsapp.R
+import com.skid.newsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        binding.activityMainBottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.bottom_menu_headlines -> TODO("Open HeadlinesFragment")
+                R.id.bottom_menu_saved -> TODO("Open SavedFragment")
+                R.id.bottom_menu_sources -> TODO("Open SourcesFragment")
+                else -> return@setOnItemSelectedListener false
+            }
+            true
+        }
     }
 }
